3d3d7b891d69430da64ea063c47c5f26365bef1b
fuyao121701 <m13739732328@163.com>
2020-01-26 13:20:11 +0800

    移动端样式reset

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import '../static/style/reset.css'
+import '../static/style/border.css'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,15 @@
+
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    hello word
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
-
 <style>
 
 </style>
