ab7e44471a820483ddd56f3ad40005301f971370
fuyao121701 <m13739732328@163.com>
2020-02-01 10:59:28 +0800

    实现header的渐隐渐现

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="box"></div>
   </div>
 </template>
 
 <script>
 import detailBanner from './components/Banner'
+import detailHeader from './components/DetailHeader'
 export default {
   name: 'Detail',
   components: {
-    detailBanner
+    detailBanner,
+    detailHeader
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  .box
+    background #25a4bb
+    height: 15rem;
 </style>
---
+++ src/pages/detail/components/DetailHeader.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="header">
+      <router-link tag="div" to="/" class="header-abs" v-show="!showHeader">
+        <span class="iconfont header-abs-icon"> &#xe61b; </span>
+      </router-link>
+      <div class="header-fixed" v-show="showHeader" :style="opacityStyle">
+        <span class="iconfont header-fixed-icon" @click="goBack"> &#xe61b; </span>
+        城市详情
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      showHeader: false,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      let top = document.documentElement.scrollTop
+      if (top > 60) {
+        this.opacityStyle.opacity = top / 140 > 1 ? 1 : top / 140
+        this.showHeader = true
+      } else {
+        this.showHeader = false
+      }
+    },
+    goBack () {
+      this.$router.push('/')
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.header-abs
+  position absolute
+  left .2rem
+  top .2rem
+  background #000
+  opacity .8
+  width .6rem
+  height .6rem
+  border-radius .3rem
+  .header-abs-icon
+    color #fff
+    line-height .6rem
+    text-align center
+    display list-item
+.header-fixed
+  position fixed
+  top 0
+  left 0
+  right 0
+  height .6rem
+  width 100%
+  background #25a4bb
+  text-align center
+  line-height .6rem
+  .header-fixed-icon
+    position absolute
+    left .2rem
+</style>
